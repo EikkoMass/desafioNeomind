b88b457ff007ba0f9d04651586dc7cffc6531315
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2020-09-27 15:36:22 -0300

    Changed Files

--- projeto2Ponteiros/src/com/projeto/me/AnguloPonteiros.java
+++ projeto2Ponteiros/src/com/projeto/me/AnguloPonteiros.java
@@ -3,66 +3,52 @@ import java.util.GregorianCalendar;
 
 public class AnguloPonteiros {
 
-	
+	/* modelo com movimentação do ponteiro da hora em relação ao minuto */
 	public long retornaAnguloRelogio(GregorianCalendar horario) {
 		
 		
-		int hora = horario.get(GregorianCalendar.HOUR_OF_DAY); //hora em 24h
-		//hora = horario.get(GregorianCalendar.HOUR); //hora em 12h (mais recomendado para o caso)
+		int hora = horario.get(GregorianCalendar.HOUR); //formato em 12h
 		int minuto = horario.get(GregorianCalendar.MINUTE); //minutos
 		
 		// campos para manipulação de data //
 		
-		hora=10;
-		minuto=34;
-		
-		// ------------------------------ //
-		
-		System.out.println(hora+":"+minuto);
-		
-		if(hora > 12) hora -= 12; //se for hora em 24h, aplica conversão para modelo em 12h
-	
-		System.out.println(hora+":"+minuto);
-		
-		long resultado = (60*hora - 11*minuto)/2;
-		
-		if(resultado < 0) resultado *= -1;
-		
-		return resultado;
-	}
-	
-	
-	/* 	Versão abaixo aplica conceito mais exato do ângulo, comparando
-		hora em 24h com hora em 12h, para identificar se modelo 24h se
-		refere a '12h' ou '00h' ('meio dia' ou 'meia noite').
-	*/
-	
-	
-	
-	/*public long retornaAnguloRelogio(GregorianCalendar horario) {
-		
-		
-		int hora24h = horario.get(GregorianCalendar.HOUR_OF_DAY); //hora em 24h
-		int hora12h = horario.get(GregorianCalendar.HOUR); //hora em 12h (mais recomendado para o caso)
-		int minuto = horario.get(GregorianCalendar.MINUTE); //minutos
-		
-		// campos para manipulação de data //
-		
-		//hora24h=12;
+		//hora=00;
 		//minuto=30;
 		
 		// ------------------------------ //
 		
-		System.out.println(hora24h+":"+minuto);
+		System.out.println(hora+":"+minuto);
 		
-		if(hora24h > 12 && hora12h != hora24h) hora24h -= 12; //se for hora em 24h, aplica conversão para modelo em 12h
+		if(hora == 12) hora = 0;
+				
+		long resultado = (11*minuto - 60*hora)/2;
+				
+		return resultado;
+	}
+	
+	/* modelo sem movimentação do ponteiro da hora em relação ao minuto 
+	public long retornaAnguloRelogio(GregorianCalendar horario) {
 		
-		System.out.println(hora24h+":"+minuto);
 		
-		long resultado = (60*hora24h - 11*minuto)/2;
+		int hora = horario.get(GregorianCalendar.HOUR); //formato em 12h
+		int minuto = horario.get(GregorianCalendar.MINUTE); //minutos
 		
-		if(resultado < 0) resultado *= -1;
+		// campos para manipulação de data //
 		
+		//hora=00;
+		//minuto=15;
+		
+		// ------------------------------ //
+		
+		System.out.println(hora+":"+minuto);
+		
+		if(hora == 12) hora = 0;
+						
+		hora *= 30;
+		minuto *= 6;
+		
+		long resultado = Math.abs(hora - minuto);
+				
 		return resultado;
 	}*/
 	
--- projeto2Ponteiros/src/com/projeto/me/Principal.java
+++ projeto2Ponteiros/src/com/projeto/me/Principal.java
@@ -1,5 +1,7 @@
 package com.projeto.me;
+
 import java.util.GregorianCalendar;
+
 
 import javax.swing.JOptionPane;
 
@@ -7,14 +9,58 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		String[] options = {"Horário Atual", "Horário Personalizado"};
+		
+		
+		int selecionaOpcao = JOptionPane.showOptionDialog(
+				null, 
+				"Qual tipo de horário você deseja?", 
+				"Angulo do Relógio", 
+				JOptionPane.OK_CANCEL_OPTION, 
+				JOptionPane.INFORMATION_MESSAGE, 
+				null, 
+				options, 
+				options[0]);
 		
 		GregorianCalendar data = new GregorianCalendar();
 		AnguloPonteiros angulo = new AnguloPonteiros();
 		
-		long resultado = angulo.retornaAnguloRelogio(data);
+		long resultado = 0;
 		
-		System.out.println("Ângulo: "+ resultado +"º ou "+ (360 - resultado)+"º");
-		JOptionPane.showMessageDialog(null, "Ângulo: "+ resultado +"º ou "+ (360 - resultado)+"º");
+		switch(selecionaOpcao) {
+		
+		case 0:
+			
+			resultado = angulo.retornaAnguloRelogio(data);
+			
+			break;
+		
+		case 1:
+			
+			int novaHora= Integer.parseInt(JOptionPane.showInputDialog("Por Favor insira a hora desejada! [1 até 12h]")); //também aceitaria 0
+			
+			while(novaHora < 1 || novaHora > 12) {
+				JOptionPane.showMessageDialog(null, "Insira um valor válido!");
+				novaHora = Integer.parseInt(JOptionPane.showInputDialog("Por Favor insira a hora desejada! [1 até 12h]"));
+			}
+			
+			int novoMinuto=Integer.parseInt(JOptionPane.showInputDialog("Por Favor insira o minuto desejado! [00 até 59]"));
+			
+			while(novoMinuto < 0 || novoMinuto > 59) {
+				JOptionPane.showMessageDialog(null, "Insira um valor válido!");
+				novoMinuto=Integer.parseInt(JOptionPane.showInputDialog("Por Favor insira o minuto desejado! [00 até 59]"));
+			}
+			
+			
+			data.set(data.get(GregorianCalendar.YEAR), data.get(GregorianCalendar.DAY_OF_MONTH), data.get(GregorianCalendar.MONTH), novaHora, novoMinuto);
+			resultado = angulo.retornaAnguloRelogio(data);
+			break;
+			
+		}
+
+		JOptionPane.showMessageDialog(null, "Ângulo: "+ resultado +"º");
+
+		System.out.println("Ângulo: "+ resultado +"º");
 
 	}
 	
